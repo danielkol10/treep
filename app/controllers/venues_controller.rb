5bc467c88423fd06b63e2352f2aebc47984526fe
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-12-02 20:45:13 +0200

    venues controller api call works

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,10 @@
 class VenuesController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,22 @@ module ApplicationHelper
     "Government" => "112",
     "Fashion" => "106"
   }
+
+  def foursquare_api(query, price, section, near)
+    # conn = Faraday.new(url: "https://api.foursquare.com/v2/venues
+    #   /explore?near=#{city}&client_id=#{ENV['FOURSQUARE_ID']}&client_secret=#{ENV['FOURSQUARE_SECRET']}&v=20181202")
+    base_url = "https://api.foursquare.com/v2/venues/"
+
+    foursquare_response = Faraday.new(url: base_url) do |f|
+      f.adapter :net_http
+      f.response :json
+    end
+
+    response = foursquare_response.get("explore/?", client_id: ENV['FOURSQUARE_ID'], client_secret: ENV['FOURSQUARE_SECRET'], near: :city, query: query, price: price, section: section)
+    raise
+    events_trip = []
+    parsed_response.each do |event|
+    end
+  end
+
 end
