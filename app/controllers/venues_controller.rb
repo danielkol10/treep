82835702d5d9fcb0bcf489d03499d32304163613
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-12-02 20:59:20 +0200

    venues new

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,9 @@
 class VenuesController < ApplicationController
   def new
-
+    @venue = Venue.new
+    query = params["search"]["food_type"]
+    # foursquare_api(query, 2, "food", Trip.last.city)
+    results = foursquare_api(query, 2, "food", "london")
   end
 
   def create
