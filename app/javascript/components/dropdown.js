42701cc781c7cd0400cfc0ab4c57b8a7dd81ffb6
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-12-04 13:20:35 +0200

    added the dropdown to the cards, with hover effects

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,18 +1,23 @@
 console.log("test")
-    const button = document.querySelector('#menu-button');
-    console.log(button);
-    let menu = document.querySelector('#menu');
+    const buttons = document.querySelectorAll('.menu-button');
+    console.log(buttons);
+    let menus = document.querySelectorAll('.menu');
+
+    console.log(menus);
     let submit = document.querySelector("#submit-button");
+
+    buttons.forEach((button) => {
     button.addEventListener('click', function (event) {
+      let menu = button.lastElementChild;
+      console.log(button)
       if (menu.style.display == "") {
         menu.style.display = "none";
-        button.innerHTML = "Show Menu";
       } else {
         menu.style.display = "";
-        button.innerHTML = "Hide Menu";
       }
     }
     );
+    });
 
     submit.addEventListener('click', function (event) {
       if (menu.style.display == "") {
