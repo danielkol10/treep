0376d7a2eebdcc6a9e4fe09f6862d8a0a3bb5d7f
Baraa Shrem <baraa.shrem@gmail.com>
2018-12-04 10:55:10 +0200

    Venues Form - API call - Venue Create

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -8,19 +8,6 @@ class VenuesController < ApplicationController
   end
 
   def new
-    # @venue = Venue.new
-    # @section = params["section"]
-    # @trip = Trip.find(params[:trip_id])
-    # @location = @trip.city
-    # @price = 2
-
-    # if !params["search"].nil? && params["search"]["food_type"]
-    #   @food_type = params["search"]["food_type"]
-    #   @venues = foursquare_api(@food_type, 2, @section, @location)
-    # end
-
-    # foursquare_api(query, 2, "food", Trip.last.city)
-    # results = foursquare_api("query", 2, section, "london")
   end
 
   def create
@@ -34,18 +21,12 @@ class VenuesController < ApplicationController
         @result = foursquare_api(FoursquareCategory.find(category).category_id, @location)
         if !@result.nil?
           @result.first(2).each do |venue|
-            @venue = Venue.new(name: venue[:name], location: venue[:address])
-            raise
+            @venue = Venue.new(name: venue[:name], location: venue[:address], trip_id: @trip.id)
             @venue.save
           end
         end
       end
     end
-
     redirect_to trip_venues_path
-  end
-
-  def venue_params
-    params.require(:venue).permit(:name, :location, :trip_id)
   end
 end
