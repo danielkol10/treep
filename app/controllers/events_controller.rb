6f0608430ab9e95bcacd93f4ebfb9d6b53cd4d59
Jack <jack.cohen1997@gmail.com>
2018-12-04 13:28:09 +0200

    working eventbrite

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,19 +2,33 @@ class EventsController < ApplicationController
   include ApplicationHelper
   def index
     @trip = Trip.find(params[:trip_id])
-    raise
+    @eventbrite_main_categories = ["Music", "Business", "Film & Media", "Science & Tech"]
+    @eventbrite_sub_categories = eventbrite_sub_categories
   end
 
-  def new
+  def create
+    @eventbrite_sub_categories = eventbrite_sub_categories
     @trip = Trip.find(params[:trip_id])
-    @event = Event.new
-    @slug = params["categories"]
     @latitude = @trip.latitude
     @longitude = @trip.longitude
+
     @start_day = @trip.start_day
     @formatted_start = @start_day.strftime('%Y-%m-%dT%H:%M:%S')
     @end_day = @trip.end_day
     @formatted_end = @end_day.strftime('%Y-%m-%dT%H:%M:%S')
-    @events = eventbrite_api(@latitude, @longitude, @formatted_start, @formatted_end, @slug)
+
+      if !params["sub_category"]["choices"].drop(1).nil?
+      @chosen_categories = params["sub_category"]["choices"].drop(1)
+      @chosen_categories.each do |category, values|
+        @result = eventbrite_api(@latitude, @longitude, @formatted_start, @formatted_end, @eventbrite_sub_categories[category][0], @eventbrite_sub_categories[category][1] )
+
+        if !@result.nil?
+          @result.first(2).each do |event|
+            @event = Event.new(name: event[:name], latitude: event[:latitude], longitude: event[:longitude], event_start: event[:event_start], event_end: event[:event_end], trip_id: @trip.id)
+            @event.save
+          end
+        end
+      end
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
     return venues_details
   end
 
-  def eventbrite_api(latitude, longitude, start_day, end_day, slug)
+  def eventbrite_api(latitude, longitude, start_day, end_day, category, sub)
     base_url = "https://www.eventbriteapi.com/v3/events/"
 
     eventbrite_response = Faraday.new(url: base_url) do |f|
@@ -25,37 +25,34 @@ module ApplicationHelper
       f.response :json
     end
 
-    response = eventbrite_response.get("search/?", token: ENV['EVENTBRITE'], "location.latitude" => latitude, "location.longitude" => longitude, "location.within" => "20mi", expand: "venue", "start_date.range_start" => start_day, "start_date.range_end" => end_day, categories: slug)
+    response = eventbrite_response.get("search/?", token: ENV['EVENTBRITE'], "location.latitude" => latitude, "location.longitude" => longitude, "location.within" => "20mi", expand: "venue", "start_date.range_start" => start_day, "start_date.range_end" => end_day, categories: category, subcategories: sub)
     events = response.body["events"]
     events_details = []
     events.each do |event|
-      events_details << { name: event["name"]["text"], latitude: event["venue"]["latitude"], longitude: event["venue"]["longitude"] }
+      events_details << { name: event["name"]["text"], latitude: event["venue"]["latitude"], longitude: event["venue"]["longitude"], event_start: event["start"]["local"],event_end: event["end"]["local"] }
     end
+    return events_details
   end
 
-  eventbrite_category_name = ["Music", "Business", "Film & Media", "Science & Tech"]
-  @eventbrite_category_id = ["103", "101", "104", "102"]
-  @eventbrite_subcategory_music = ["3002", "3003", "3006", "3008"]
-  @eventbrite_subcategory_business = ["1001", "1002", "1005", "1007"]
-  @eventbrite_subcategory_film = ["4004", "4003", "4002", "4005"]
-  @eventbrite_subcategory_science = ["2001", "2002", "2003", "2004"]
-
-  @eventbrite_category = {
-    "Jazz & Blues" => ["103", "3002"],
-    "Classical" => ["103", "3003"],
-    "EDM / Electronic" => ["103", "3006"],
-    "HipHop / Rap" => ["103", "3008"],
-    "Startups & Small Business" => ["101", "1001"],
-    "Finance" => ["101", "1002"],
-    "Real Estate" => ["101", "1005"],
-    "Sales & Marketing" => ["101", "1007"],
-    "Gaming" => ["104", "4004"],
-    "Anime" => ["104", "4003"],
-    "Film" => ["104", "4002"],
-    "Comics" => ["104", "4005"],
-    "Medicine" => ["102", "2001"],
-    "Science" => ["102", "2002"],
-    "Biotech" => ["102", "2003"],
-    "High Tech" => ["102", "2004"]
-  }
+  def eventbrite_sub_categories
+    @eventbrite_sub_categories = {
+      "Jazz & Blues" => ["103", "3002"],
+      "Classical" => ["103", "3003"],
+      "EDM / Electronic" => ["103", "3006"],
+      "HipHop / Rap" => ["103", "3008"],
+      "Startups & Small Business" => ["101", "1001"],
+      "Finance" => ["101", "1002"],
+      "Real Estate" => ["101", "1005"],
+      "Sales & Marketing" => ["101", "1007"],
+      "Gaming" => ["104", "4004"],
+      "Anime" => ["104", "4003"],
+      "Film" => ["104", "4002"],
+      "Comics" => ["104", "4005"],
+      "Medicine" => ["102", "2001"],
+      "Science" => ["102", "2002"],
+      "Biotech" => ["102", "2003"],
+      "High Tech" => ["102", "2004"]
+    }
+    return @eventbrite_sub_categories
+  end
 end
