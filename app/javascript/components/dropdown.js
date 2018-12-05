79432ec3770cee1d4a5af4e753b8d75246ad57f1
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2018-12-05 20:49:09 +0200

    closes current card, but doesnt open next one

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,26 +1,27 @@
     const buttons = document.querySelectorAll('.menu-button');
     let menus = document.querySelectorAll('.menu');
 
-    // console.log(menus);
     let submit = document.querySelectorAll(".submit-venue-category");
 
     buttons.forEach((button) => {
-    button.addEventListener('click', function (event) {
-      let menu = button.nextElementSibling;
-      console.log(menu)
-      menu.classList.toggle("hide-start");
-    }
-    );
+      button.addEventListener('click', function(event) {
+        let menu = button.nextElementSibling;
+        menu.classList.toggle("hide-start");
+      }
+      );
     });
 
 
+    const allSubmitButtons = document.querySelectorAll('.submit-category-button');
+    console.log(buttons.value);
+    console.log(submit.value);
+    allSubmitButtons.forEach((button) => {
+      button.addEventListener('click', function(event) {
+        menus.forEach((menu) => {
+          if (button.value === submit.value) {
+          menu.classList.add("hide-start");
+          }
+        });
+      });
+    });
 
-
-    // submit.addEventListener('click', function (event) {
-    //   if (menu.style.display == "") {
-    //     menu.style.display = "none";
-    //   } else {
-    //     menu.style.display = "";
-    //   }
-    // });
-// button.disabled=true
