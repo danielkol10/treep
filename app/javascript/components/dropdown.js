b88283b5594708d4d9e532c1dd90b117a0fb7579
baraashrem <baraa.shrem@gmail.com>
2018-12-05 15:41:53 +0200

    Revert "Trips show dynamic events - check merge with Maria because of JS changes!!"

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,11 +1,11 @@
-     const buttons = document.querySelectorAll('.menu-button');
-
+console.log("test")
+    const buttons = document.querySelectorAll('.menu-button');
+    console.log(buttons);
     let menus = document.querySelectorAll('.menu');
 
-
+    console.log(menus);
     let submit = document.querySelector("#submit-button");
 
-    if(buttons.length > 0) {
     buttons.forEach((button) => {
     button.addEventListener('click', function (event) {
       let menu = button.lastElementChild;
@@ -15,7 +15,6 @@
     );
     });
 
-
     // submit.addEventListener('click', function (event) {
     //   if (menu.style.display == "") {
     //     menu.style.display = "none";
@@ -24,7 +23,3 @@
     //   }
     // });
 // button.disabled=true
-
-
-
-export { dropDown };
